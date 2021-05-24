523a831d61932f1af95e59803c10f7bf65c5d908
Julia Melnik <julls.hm.md@gmail.com>
2021-05-24 13:40:40 +0300

    правки

--- src/js/changingToolbar.js
+++ src/js/changingToolbar.js
@@ -7,21 +7,28 @@ export const bodyRef = document.querySelector('body');
 export const toolbarRef = document.querySelector('.theme-switch__toggle');
   // DEFAULT
 export const addToolbarListener = toolbarRef.addEventListener('change', onChangingToolbar);
-export const defaultTheme = bodyRef.classList.add(Theme.LIGHT);
+
 export const previousTheme = localStorage.getItem('previousTheme');
 
 // FN
 export function onChangingToolbar(evt) {
-  bodyRef.classList.toggle(Theme.LIGHT);
+
   bodyRef.classList.toggle(Theme.DARK);
    
   if (evt.currentTarget.checked) {
     localStorage.setItem('previousTheme', Theme.DARK)
+    bodyRef.classList.remove(Theme.LIGHT) 
   } else {
     localStorage.setItem('previousTheme', Theme.LIGHT)
+    bodyRef.classList.add(Theme.LIGHT);
   }
 }
 if (previousTheme===Theme.DARK) {
   toolbarRef.checked = true;
   bodyRef.classList.add(Theme.DARK)
+
   }
+if(previousTheme!==Theme.DARK){
+    toolbarRef.checked = false;
+    bodyRef.classList.add(Theme.LIGHT) 
+  } 
